e6818a95ef043d36b875f5900149692cccee7b82
elmetal <nm7.pupu@gmail.com>
2026-05-06 11:03:02 +0900

    PokemonGeneration

---
+++ Sources/PokedexAbility/PokemonGeneration.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonGeneration.swift
+//  PokedexAbility
+//
+//  Created by elmetal on 2026/05/06
+//
+//
+
+public enum PokemonGeneration: String, Hashable, Codable, Sendable {
+    case iii
+    case iv
+    case v
+    case vi
+    case vii
+    case viii
+    case ix
+    case champions
+}
